e44b43ce0abcde0eb2d586461cdbeb1a3580e2fd
Julie Gorshkova <55894804+yuagorshkova@users.noreply.github.com>
2022-10-14 22:09:23 +0300

    Update BM25_search.py

--- hw4/BM25_search.py
+++ hw4/BM25_search.py
@@ -9,18 +9,18 @@ class Search25:
 
     def get_query_vector(self, queries: list):
         """
-        :param query:
+        :param queries:
         :return: BoW representation of the query in csc
         """
         preprocessed_queries = [self.inverted_index.preprocessor.preprocess_text(query) for query in queries]
         return self.inverted_index.count_vectorizer.transform(preprocessed_queries).transpose()
 
-    def get_bm25_distance(self, query_vector: np.ndarray):
+    def get_closeness(self, query_vectors: np.ndarray):
         """
-        :param query_vector:
+        :param query_vectors:
         :return: array of BM25 values computed between query and each document
         """
-        return (self.inverted_index.matrix * query_vector).toarray()
+        return (self.inverted_index.matrix * query_vectors).toarray()
 
     def find_closest_docs(self, query: str, n: int = None):
         """
@@ -32,6 +32,6 @@ class Search25:
             n = self.inverted_index.matrix.shape[0]
 
         query_vector = self.get_query_vector([query])
-        distances = self.get_bm25_distance(query_vector).ravel()
+        distances = self.get_closeness(query_vector).ravel()
         sorted_indices = np.argsort(distances)[:-n-1:-1]
         return self.inverted_index.filename_index[sorted_indices]
